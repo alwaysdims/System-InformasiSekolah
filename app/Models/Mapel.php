f57f56be85b1283d4c412e23ba3999c7454fcde2
fiyonaa <vionav204@gmail.com>
2025-10-10 13:26:12 +0700

    ya

--- app/Models/Mapel.php
+++ app/Models/Mapel.php
@@ -7,13 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Mapel extends Model
 {
     protected $table = 'mapel';
-<<<<<<< HEAD
 
     protected $fillable = ['kode_mapel', 'nama_mapel'];
-=======
     protected $fillable = ['nama_mapel'];
     // protected $table = 'mapel';
->>>>>>> eda5ed08b53d3be2203b293c625e7f18ae5cea6e
 
     public function guru()
     {
